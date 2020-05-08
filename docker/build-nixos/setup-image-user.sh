2cd93b998c1ddbbd0581c28ee421cbad09c547e5
Roberto Frenna <roberto.frenna@gmail.com>
2020-05-08 05:30:26 +0200

    Do not error out if the cleanup container is not running

--- docker/build-nixos/setup-image-user.sh
+++ docker/build-nixos/setup-image-user.sh
@@ -14,7 +14,7 @@ echo '. /home/nixos/.nix-profile/etc/profile.d/nix.sh' >> setup-env
 # 2. force Nix to use cloned checkout, rather than the default expressions in ~/.nix-defexpr
 echo 'export NIX_PATH=$HOME' >> setup-env # forces to use cloned checkout rather than defexpr
 # 3. send a signal to `cleanup-qemu` when done
-echo 'trap "echo build done | nc cleanup-qemu 11111" INT TERM EXIT' >> setup-env
+echo 'trap "echo build done | nc cleanup-qemu 11111 2>/dev/null || echo cleanup container not running" INT TERM EXIT' >> setup-env
 
 echo "cloning nix packages..."
 git clone --depth=1 -b "$NIXPKGS_BRANCH" "$NIXPKGS_URL" nixpkgs
