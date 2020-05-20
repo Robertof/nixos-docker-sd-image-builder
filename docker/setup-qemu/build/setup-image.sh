181674e1833b3140d2b5e9dd0f69e459574374be
Roberto Frenna <roberto.frenna@gmail.com>
2020-05-21 01:15:39 +0200

    Migrate QEMU package from Debian sources to Fedora
    The Debian package was fetched directly from the mirrors, which obviously
    only retain the latest version of it. Unfortunately this means that they
    will irremediably break when an updated package is pushed.
    
    This makes it so that the package is fetched from Fedora's `kojipkgs`
    which looks like a reliable and stable source for fixed package versions.
    
    Due to this, the builder now uses `bsdtar` to extract the contents of the
    `rpm` file, and drops the dependency of `dpkg`.
    
    Fixes #2.

--- docker/setup-qemu/build/setup-image.sh
+++ docker/setup-qemu/build/setup-image.sh
@@ -4,7 +4,7 @@ set -eu
 echo "downloading QEMU..."
 wget "$QEMU_PKG_URL"
 
-QEMU_PKG=qemu*.deb
+QEMU_PKG=qemu*.rpm
 
 echo "verifying hash of" $QEMU_PKG "..."
 # note: spaces matter here!
@@ -15,6 +15,6 @@ sha256sum -c sha256 || exit 1
 echo "extracting..."
 # avoid extracting the whole thing (which is pretty big uncompressed), only retrieve the AArch64
 # binary.
-dpkg --fsys-tarfile $QEMU_PKG | tar xvF - ./usr/bin/qemu-aarch64-static --strip=3
+bsdtar xvf $QEMU_PKG --strip-components=3 ./usr/bin/qemu-aarch64-static
 
 echo "image is ready"
